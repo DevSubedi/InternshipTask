507adfca3ea171843a12161dc29dcf1627a7e820
Dev Subedi <Devsubedi10@gmail.com>
2025-04-23 00:17:30 +0545

    Made function for adding details, included picture, and finaly layout and ui adjustments

--- lib/license.dart
+++ lib/license.dart
@@ -35,169 +35,98 @@ class MyWidget extends StatelessWidget {
                         width: 50,
                         height: 50,
                       ),
+                      Column(
+                        children: [
+                          Text(
+                            'नेपाल सरकार\nGOVERNMENT OF NEPAL',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 6),
+                          Text(
+                            'सवारी चालक अनुमतिपत्र\nDRIVING LICENSE',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                       Image.asset('assets/flag.png', width: 50, height: 50),
                     ],
                   ),
                   SizedBox(height: 5),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      '03-06-01207312',
 
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  '03-06-01207312',
+
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              infoText('Name:  ', 'Dev Subedi'),
+                              infoText('Parent/Spouse:  ', 'Radheshyam Subedi'),
+                              infoText('Citizenship:  ', '27-01-75-01603'),
+                              infoText('Date of Birth:  ', "2059-01-29"),
+                              infoText('Passport No:  ', ''),
+                            ],
+                          ),
+                          Spacer(),
+
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Image.asset(
+                              'assets/pic.jpg',
+                              height: 90,
+                              width: 90,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
 
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Name:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          'Dev Subedi',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                      infoText(
+                        'Address:  ',
+                        'Bhairabmandir-5, Kathmandu, Bagmati',
+                      ),
+                      Row(
+                        children: [
+                          infoText('Issue Date:  ', '2022-08-29'),
+                          Spacer(),
+                          infoText('Expiry Date:  ', '2027-08-28'),
+                        ],
+                      ),
 
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text(
-                          'Parent/Spouse:  ',
-                          style: TextStyle(fontSize: 10),
-                        ),
-                        Text(
-                          'RadheShyam Subedi',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
+                      Row(
+                        children: [
+                          infoText('Blood Group:  ', 'A+'),
+                          Spacer(),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: infoText('Category:  ', 'A'),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Citizenship:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          '27-01-75-01603',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Date of Birth: ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          '2050-01-29',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Passport No: ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          '4359825',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Address:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          'Bhairabmandir-5, Kathmandu, Bagmati',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Issue Date:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          '2022-08-29',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                        Spacer(),
-                        Text('Expiry Date:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          '2027-08-28',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Text('Blood Group:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          'A+',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                        Spacer(),
-                        Text('Category:  ', style: TextStyle(fontSize: 10)),
-                        Text(
-                          'A',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
+                    ],
                   ),
 
                   SizedBox(height: 6),
@@ -229,7 +158,17 @@ Widget buildBox(String label) {
         ),
       ),
       SizedBox(height: 4),
-      Text(label, style: TextStyle(fontSize: 10)),
+      Text(label, style: TextStyle(fontSize: 8)),
+    ],
+  );
+}
+
+Widget infoText(String question, String answer) {
+  return Row(
+    children: [
+      Text(question, style: TextStyle(fontSize: 10)),
+      Text(answer, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10)),
+      SizedBox(height: 4),
     ],
   );
 }
